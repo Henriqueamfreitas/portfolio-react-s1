6fc538fb4c9bf5052f1b00a6921884ff3f890ffd
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-04-09 10:12:41 -0300

    revisao estilização header e estilizacao bannersection

--- src/components/AboutMeSection/index.jsx
+++ src/components/AboutMeSection/index.jsx
@@ -2,9 +2,9 @@ import styles from "../AboutMeSection/style.module.css";
 
 export const AboutMeSection = () => {
     return(
-        <section id="aboutMeSection">
-            <h2 className="title two secondary">Sobre mim</h2>
-            <p className="paragraph one secondary">
+        <section className={styles.section} id="aboutMeSection">
+            <h2 className={`${styles.h2} title two secondary`}>Sobre mim</h2>
+            <p className={`${styles.p} paragraph one secondary`}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ex magna, imperdiet sodales sem quis, 
                 sollicitudin lobortis purus. Etiam a ipsum finibus, dictum leo non, ultrices dui. Nunc id felis pharetra, 
                 vehicula enim in, suscipit nisi. Mauris eget sapien a velit facilisis ullamcorper feugiat nec orci. Duis 
